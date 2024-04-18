b92c4862c28ed1667bf06e8c350b26b09ffd3e0f
omwitsa-etole <omwitsaetole@gmail.com>
2024-04-18 09:22:02 +0300

    images fix

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ const Navbar = () => {
   return (
     <div className="navbar">
       <a href="/">
-        <img src="/assets/logo.png" alt="logo" />
+        <img src={`${process.env.PUBLIC_URL}/assets/logo.png`} alt="logo" />
       </a>
 
       <div className="navbar_search">
